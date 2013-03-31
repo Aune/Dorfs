ed6c966c5838d46b21c9c3350de5e75b476d7ad7
Caj Larsson <caj@omnicloudapp.com>
2013-03-31 23:25:53 +0200

    Made the view into a typeclass, it needs some more functions, for instance to update it.

--- src/Rendering/Test.hs
+++ src/Rendering/Test.hs
@@ -1,5 +1,6 @@
 module Main where
 
+import Rendering.SimpleMapView
 import Rendering.View
 import Map.Codec
 import Graphics.UI.SDL as SDL
@@ -11,7 +12,7 @@ main = do
    SDL.setVideoMode 640 480 32 []
    
    let exampleMap = fromString "[[0,0,0],[4,4,0]] 2345\n1 345\n12 45\n123 5\n1234 "
-   let view = View {top = 0, left = 0, width = 5, height = 5, level = 0, world = exampleMap}
+   let view = SimpleMapView {top = 0, left = 0, width = 5, height = 5, level = 0, world = exampleMap}
 
    SDL.setCaption "Video Test!" "video test"
    mainSurf <- getVideoSurface
--- src/Rendering/View.hs
+++ src/Rendering/View.hs
@@ -1,49 +1,10 @@
 module Rendering.View where
 
-import Map.Datatypes
+import Graphics.UI.SDL (Surface, Rect)
 
-import Graphics.UI.SDL as SDL
-import Graphics.UI.SDL.Image as SDLi
-
+voxelSize :: Int
 voxelSize = 16
 
-data View = View { top :: Int,
-                   left :: Int,
-                   width :: Int,
-                   height :: Int,
-                   level :: Int,
-                   world :: IO Map}
-            
-right v = (left v) + (width v) - 1
-bottom v = (top v) + (height v) - 1 
-
-render :: View -> SDL.Surface -> IO ()
-render v surface =  do mapM_ (blitPos v surface) (paintRange v)
-                       return ()
-  where 
-    paintRange :: View -> [Pos]
-    paintRange v = [(Pos x y (level v)) | 
-                    y <- [(top v)..(bottom v)],
-                    x <- [(left v)..(right v)] ]   
- 
-          
-    posToRect :: Pos -> View -> Rect
-    posToRect (Pos x y z) v = Rect ((x - (left v)) * voxelSize) 
-                                   ((y - (top v)) * voxelSize)  
-                                   voxelSize voxelSize
-        
-    tileRect :: Tile -> Rect
-    tileRect Tile {status = Open} = Rect 176 208 voxelSize voxelSize
-    tileRect _ = Rect 112 176 voxelSize voxelSize
-                                  
-    tileSurface = SDLi.load "res/test/Phoebus_16x16.png"
-                 
-    blitTile :: Tile -> Rect -> SDL.Surface-> SDL.Surface -> IO ()
-    blitTile t r src dst = do SDL.blitSurface src (Just (tileRect t))  dst (Just r) 
-                              return ()
-                        
-    blitPos :: View -> SDL.Surface -> Pos -> IO ()
-    blitPos v dst p  =  do 
-      tile <- (world v) >>= tileAtPos p       
-      src <- tileSurface
-      blitTile tile (posToRect p v) src dst
+class View a where            
+  render :: a -> Surface -> IO ()
+   
